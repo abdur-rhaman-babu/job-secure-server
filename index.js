ad6468cb2101c165bbae37eb0c8607fb6ffa4269
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-21 11:45:43 +0600

    get single job for update

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
           res.send(result)
       })
 
+      // get job for update
+      app.get('/update/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await jobsCollections.findOne(query)
+        res.send(result)
+      })
+
   } finally {
     // Ensures that the client will close when you finish/error
   }
