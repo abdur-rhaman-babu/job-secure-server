6aafbe24724218caf8b02fb61cf3aa898e962743
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-27 12:54:38 +0600

    bid count creaseing

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const port = process.env.PORT || 9000
-const app = express()
+const port = process.env.PORT || 9000;
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,94 +17,105 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 
 async function run() {
   try {
-      const db= client.db('jobsDB')
-      const jobsCollections = db.collection('jobs')
-      const bidsCollections = db.collection('bids')
+    const db = client.db("jobsDB");
+    const jobsCollections = db.collection("jobs");
+    const bidsCollections = db.collection("bids");
 
-      // post or create jobs from the client
-      app.post('/jobs', async (req, res)=>{
-        const newJob = req.body;
-        const result = await jobsCollections.insertOne(newJob)
-        res.send(result)
-      })
+    // post or create jobs from the client
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollections.insertOne(newJob);
+      res.send(result);
+    });
 
-      // get job form server
-      app.get('/jobs', async (req, res)=>{
-        const result = await jobsCollections.find().toArray()
-        res.send(result)
-      })
+    // get job form server
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollections.find().toArray();
+      res.send(result);
+    });
 
-      // get data wiht email system-01
-      app.get('/jobs/:email', async (req, res)=>{
-          const email = req.params.email;
-          const query = {'buyer.email': email}
-          const result = await jobsCollections.find(query).toArray()
-          res.send(result)
-      })
-      
-      // // get data wiht email system-02
-      // app.get('/myPostedJob', async (req, res)=>{
-      //     const email = req.query.email;
-      //     const query = {'buyer.email': email}
-      //     const result = await jobsCollections.find(query).toArray()
-      //     res.send(result)
-      // })
+    // get data wiht email system-01
+    app.get("/jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "buyer.email": email };
+      const result = await jobsCollections.find(query).toArray();
+      res.send(result);
+    });
 
-      // delete job from db
-      app.delete('/job/:id', async (req, res)=>{
-          const id = req.params.id;
-          const query = {_id: new ObjectId(id)}
-          const result = await jobsCollections.deleteOne(query)
-          res.send(result)
-      })
+    // // get data wiht email system-02
+    // app.get('/myPostedJob', async (req, res)=>{
+    //     const email = req.query.email;
+    //     const query = {'buyer.email': email}
+    //     const result = await jobsCollections.find(query).toArray()
+    //     res.send(result)
+    // })
 
-      // get job for update
-      app.get('/update/:id', async (req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await jobsCollections.findOne(query)
-        res.send(result)
-      })
+    // delete job from db
+    app.delete("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.deleteOne(query);
+      res.send(result);
+    });
 
-      // post job with update
-      app.post('/update/:id', async (req, res)=>{
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)}
-        const job = req.body;
-        const updatedJob = {
-          $set: job
-        }
-        const options = {upsert: true}
-        const result = await jobsCollections.updateOne(filter, updatedJob, options)
-        res.send(result)
-      })
+    // get job for update
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.findOne(query);
+      res.send(result);
+    });
 
-      // get single job for details page
-      app.get('/details/:id', async (req, res)=>{
-          const id = req.params.id;
-          const query = {_id: new ObjectId(id)}
-          const result = await jobsCollections.findOne(query)
-          res.send(result)
-      })
+    // post job with update
+    app.post("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const job = req.body;
+      const updatedJob = {
+        $set: job,
+      };
+      const options = { upsert: true };
+      const result = await jobsCollections.updateOne(
+        filter,
+        updatedJob,
+        options
+      );
+      res.send(result);
+    });
 
-      // post bid api
-      app.post('/add-bid', async (req, res)=>{
-        const bidData = req.body;
-        const result = await bidsCollections.insertOne(bidData)
-        res.send(result)
-      })
+    // get single job for details page
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.findOne(query);
+      res.send(result);
+    });
 
+    // post bid api
+    app.post("/add-bid", async (req, res) => {
+      const bidData = req.body;
+      const result = await bidsCollections.insertOne(bidData);
+
+      // bid count
+      const filter = { _id: new ObjectId(bidData.jobId) };
+      const update = {
+        $inc: { bid_count: 1 },
+      };
+      const updateBid = await jobsCollections.updateOne(filter, update);
+      console.log(updateBid)
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir)
-app.get('/', (req, res) => {
-  res.send('Hello from SoloSphere Server....')
-})
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello from SoloSphere Server....");
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
